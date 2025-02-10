45593343de2f869bad669bbe7ce2e723475bed83
Lgtf5 <lgtablasf5@gmail.com>
2025-02-10 16:44:29 +0100

    resolvimos nombres del método CalculadoraMCI a calcularMCI para aclarar que es el método para valores introducidos admitidos

--- src/test/java/dev/lidia/mci/MyComponent/CalculadoraMCITest.java
+++ src/test/java/dev/lidia/mci/MyComponent/CalculadoraMCITest.java
@@ -9,14 +9,14 @@ public class CalculadoraMCITest {
 
     @org.junit.jupiter.api.Test
     @DisplayName ("testar que el resultado de la fórmula no de negativos y que devuelva el resultado de la fórmula IMC")
-    public void testCalculadoraMCI() {
+    public void testCalcularMCIValoresNormales() {
         //datos persona ficticia
         double peso= 70;
         double estatura= 1.75;
         double resultadoEsperado = peso / Math.pow (estatura, 2);
 
         //asignamos como concepto "resultado" al resultado de la formulación descrita en la clase
-        double resultado = CalculadoraMCI.CalculadoraMCI(peso, estatura);
+        double resultado = CalculadoraMCI.calcularMCI(peso, estatura);
 
         //contraste que el resultado en sí sea el resultado ficticio esperado (0.01 es una convención que se crea en formulación matemática como rango de error mínimo)
 
@@ -24,4 +24,5 @@ public class CalculadoraMCITest {
         assertThat(resultado, is(closeTo(resultadoEsperado, 0.01)));
     }
     
+   
 }
